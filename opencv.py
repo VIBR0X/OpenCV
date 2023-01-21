2acee5e642e50b846a771ca3c105aa2e0074c746
Vedant Thakre <44766005+vedant-github@users.noreply.github.com>
2023-01-21 16:01:48 +0530

    transform

--- opencv.py
+++ opencv.py
@@ -1,12 +1,15 @@
 import cv2 as cv
 import numpy as np
+import torch
+import torchvision as transform
 
-vid = cv.VideoCapture(0)
+
+#vid = cv.VideoCapture(0)
 
 #### importing and reading image
-# img = cv.imread("D:\opencv\Asus.jpg")
+# img = cv.imread("Asus.jpg")
 # cv.imshow('A', img)
-#cv.waitKey(0)
+# cv.waitKey(0)
 
 #### Using webcam
   
@@ -21,24 +24,32 @@ vid = cv.VideoCapture(0)
 # vid.release()
 # cv.destroyAllWindows()
 
+#### 10 alternative images
+
+img1 = cv.imread("t.jpg")
+cv.imshow('img', img1)
+trans1 = transform.Resize(40,40)
+cv.imshow('trans1', trans1)
+cv.waitKey(0)
+
+
 #### Detecting blue in video
 
-
-while(1):
-    # Take each frame
-    _, frame = vid.read()
-    # Convert BGR to HSV
-    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    # define range of blue color in HSV
-    lower_blue = np.array([110,50,50])
-    upper_blue = np.array([130,255,255])
-    # Threshold the HSV image to get only blue colors
-    mask = cv.inRange(hsv, lower_blue, upper_blue)
-    # Bitwise-AND mask and original image
-    res = cv.bitwise_and(frame,frame, mask= mask)
-    cv.imshow('frame',frame)
-    cv.imshow('mask',mask)
-    cv.imshow('res',res)
-    if cv.waitKey(1) & 0xFF == ord('q'):
-      break
-cv.destroyAllWindows()
+# while(1):
+#     # Take each frame
+#     _, frame = vid.read()
+#     # Convert BGR to HSV
+#     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+#     # define range of blue color in HSV
+#     lower_blue = np.array([110,50,50])
+#     upper_blue = np.array([130,255,255])
+#     # Threshold the HSV image to get only blue colors
+#     mask = cv.inRange(hsv, lower_blue, upper_blue)
+#     # Bitwise-AND mask and original image
+#     res = cv.bitwise_and(frame,frame, mask= mask)
+#     cv.imshow('frame',frame)
+#     cv.imshow('mask',mask)
+#     cv.imshow('res',res)
+#     if cv.waitKey(1) & 0xFF == ord('q'):
+#       break
+# cv.destroyAllWindows()
