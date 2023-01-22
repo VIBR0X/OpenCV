a1cd317aeabcfd0d52344d1dc25f42ab3667cc4b
Vedant Thakre <44766005+vedant-github@users.noreply.github.com>
2023-01-22 14:26:38 +0530

    video object detection

--- opencv.py
+++ opencv.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+import matplotlib.pyplot as plt
 import torch
 import torchvision as transform
 
@@ -24,13 +25,108 @@ import torchvision as transform
 # vid.release()
 # cv.destroyAllWindows()
 
-#### 10 alternative images
+#### image transform
 
-img1 = cv.imread("t.jpg")
-cv.imshow('img', img1)
-trans1 = transform.Resize(40,40)
-cv.imshow('trans1', trans1)
-cv.waitKey(0)
+# img1 = cv.imread("t.jpg")
+# cv.imshow('img', img1)
+# grey = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
+# #cv.imshow('grey', grey)
+# blur = cv.GaussianBlur(img1, (5,5), cv.BORDER_DEFAULT)
+# #cv.imshow('blur',blur)
+# canny = cv.Canny(img1, 100, 200)
+# cv.imshow('canny', canny)
+# cv.waitKey(0)
+
+
+#### Detecting Shapes
+
+# img = cv.imread('shapes.png')
+  
+# gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+  
+# _, threshold = cv.threshold(gray, 127, 255, cv.THRESH_BINARY)
+  
+
+# contours, _ = cv.findContours(
+#     threshold, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+  
+# i = 0
+  
+# # list for storing names of shapes
+# for contour in contours:
+  
+  
+#     if i == 0:
+#         i = 1
+#         continue
+    
+#     approx = cv.approxPolyDP(
+#         contour, 0.01 * cv.arcLength(contour, True), True)
+      
+    
+#     cv.drawContours(img, [contour], 0, (0, 0, 255), 5)
+  
+#     M = cv.moments(contour)
+#     if M['m00'] != 0.0:
+#         x = int(M['m10']/M['m00'])
+#         y = int(M['m01']/M['m00'])
+  
+#     # putting shape name at center of each shape
+#     if len(approx) == 3:
+#         cv.putText(img, 'Triangle', (x, y),
+#                     cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+  
+#     elif len(approx) == 4:
+#         cv.putText(img, 'Quadrilateral', (x, y),
+#                     cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+  
+#     elif len(approx) == 5:
+#         cv.putText(img, 'Pentagon', (x, y),
+#                     cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+  
+#     elif len(approx) == 6:
+#         cv.putText(img, 'Hexagon', (x, y),
+#           cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+  
+#     else:
+#         cv.putText(img, 'circle', (x, y),
+#                     cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+  
+# cv.imshow('shapes', img)
+  
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+
+
+#### Detecting Ball in the video
+vid = cv.VideoCapture('messi.mp4')
+prevc = None
+dist = lambda x1,y1,x2,y2: (x1-x2)**2+(y1-y2)**2
+while(True):
+    ret, frame = vid.read()
+    grey = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    blur = cv.GaussianBlur(grey, (17,17), cv.BORDER_DEFAULT)
+    circle = cv.HoughCircles(blur, cv.HOUGH_GRADIENT, 1.3, 1000, param1=100, param2=40)
+
+    if circle is not None:
+        circle = np.uint16(np.around(circle))
+        sel = None
+        for i in circle[0,:]:
+            if sel is None: sel = i
+            if prevc is not None:
+                if dist(sel[0],sel[1],prevc[0],prevc[1]) <= dist(i[0],i[1],prevc[0],prevc[1]):
+                    sel = i
+        cv.circle(frame, (sel[0],sel[1]), 2, (100,0,100), 2 )
+        cv.circle(frame, (sel[0],sel[1]), sel[2], (255,0,0), 4 )
+        prevc = sel
+  
+    cv.imshow('frame', frame)
+
+    if cv.waitKey(60) & 0xFF == ord('q'):
+        break
+
+vid.release()
+cv.destroyAllWindows()
 
 
 #### Detecting blue in video
